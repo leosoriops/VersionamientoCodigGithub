89c7b3451ce4f794891687536ade8ae34fca0ccd
Viviana Osorio <leosorio@personalsoft.com.co>
2022-05-15 23:17:41 -0500

    tructura inical cabecera Please enter the commit message for your changes. Lines starting

--- CromulentBisgetti.DemoApp/Program.cs
+++ CromulentBisgetti.DemoApp/Program.cs
@@ -19,6 +19,6 @@ namespace CromulentBisgetti.DemoApp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>(    );
+                .UseStartup<Startup>(  );
     }
 }
